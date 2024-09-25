09673446b88f2377211db8b19c4beaedf8a113fd
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2024-09-25 11:31:48 +0200

    Added springBoot

---
+++ src/main/java/com/example/demo/DemoExpass2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExpass2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExpass2Application.class, args);
+	}
+
+}
