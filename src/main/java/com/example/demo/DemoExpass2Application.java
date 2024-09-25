d399f126ace37a21d971cb1e732f69c0a319478a
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2024-09-25 12:06:40 +0200

    Application running

--- src/main/java/com/example/demo/DemoExpass2Application.java
+++ src/main/java/com/example/demo/DemoExpass2Application.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoExpass2Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoExpass2Application.class, args);
-	}
-
+    public static void main(String[] args) {
+      SpringApplication.run(DemoExpass2Application.class, args);
+    }
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
